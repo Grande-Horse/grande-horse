db28ff427d2694f7d5ce7f62e8ea3091c4e86712
신성우 <asdfgg77@naver.com>
2025-03-18 09:39:40 +0900

    chore: 목장 영어 명칭 통일로 인한 수정, 배경색 적용

--- frontend/web/src/layouts/GlobalLayout.tsx
+++ frontend/web/src/layouts/GlobalLayout.tsx
@@ -1,5 +1,6 @@
-import BottomNavBar from '@/components/ui/navbar/BottomNavBar';
 import { ReactNode } from 'react';
+import Header from '@/components/ui/header/Header';
+import BottomNavBar from '@/components/ui/navbar/BottomNavBar';
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,6 +8,7 @@ interface LayoutProps {
 
 const GlobalLayout: React.FC<LayoutProps> = ({ children }) => (
   <div className='bg-background max-w-base font-primary text-body2 m-auto flex min-h-screen flex-col justify-between text-white'>
+    <Header />
     <main className='flex-1 overflow-auto'>{children}</main>
     <BottomNavBar variant='default' />
   </div>
