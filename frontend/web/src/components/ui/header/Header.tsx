22c45cf01914cf5dd90bcaad2198a05381dc8995
신성우 <asdfgg77@naver.com>
2025-03-18 10:38:20 +0900

    fix: 용어 통일로 인한 수정, 배경색상 적용

--- frontend/web/src/components/ui/header/Header.tsx
+++ frontend/web/src/components/ui/header/Header.tsx
@@ -1,11 +1,11 @@
-import { DefaultContent, InfoContent, RanchContent, WaitingRoomContent } from '@/components/ui/header/HeaderContent';
+import { DefaultContent, InfoContent, PastureContent, WaitingRoomContent } from '@/components/ui/header/HeaderContent';
 
 interface HeaderProps {
-  variant?: 'default' | 'info' | 'waitingRoom' | 'ranch';
+  variant?: 'default' | 'info' | 'waitingRoom' | 'pasture';
   title?: string;
 }
 
-const commonStyle = 'flex items-center justify-between p-5 sticky top-0 z-10 w-full';
+const commonStyle = 'flex items-center justify-between p-5 sticky top-0 z-10 w-full bg-background';
 
 const Header: React.FC<HeaderProps> = ({ variant = 'default', title }) => {
   const coin = 100000;
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ variant = 'default', title }) => {
     default: <DefaultContent coin={coin} foot={foot} />,
     info: <InfoContent title={title} />,
     waitingRoom: <WaitingRoomContent title={title} />,
-    ranch: <RanchContent coin={coin} foot={foot} />,
+    pasture: <PastureContent coin={coin} foot={foot} />,
   };
 
   return <header className={commonStyle}>{component[variant]}</header>;
