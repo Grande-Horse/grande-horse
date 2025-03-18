435875ff06d038c46d8bb83e695493d6624eb26e
신성우 <asdfgg77@naver.com>
2025-03-18 15:40:49 +0900

    feat(S12P21A606-29): 방 리스트 UI 컴포넌트 구현

---
+++ frontend/web/src/components/raceTrack/RoomItem.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import HeadCountIcon from '@/assets/icons/headCountIcon.svg?react';
+import CoinIcon from '@/assets/icons/coinIcon.svg?react';
+import { rankColor, rankName } from '@/constants/rank';
+import { type RoomData } from '@/types/room';
+
+interface RoomItemProps {
+  room: RoomData;
+}
+
+const RoomItem: React.FC<RoomItemProps> = ({ room }) => {
+  return (
+    <Link
+      to={'/'}
+      className='flex items-center justify-between gap-5 rounded-xl bg-white/10 p-9 inset-shadow-xs inset-shadow-white/30 hover:bg-white/20 active:bg-white/30'
+    >
+      <p className='flex-1 truncate'>{room.title}</p>
+      <div className='text-stroke flex items-center gap-3'>
+        <p className={`${rankColor[room.rank]}`}>{rankName[room.rank]} ↓</p>
+        <div className='flex items-center gap-2'>
+          <CoinIcon className='size-6' />
+          <p className='flex w-12 justify-end'>{room.batting}</p>
+        </div>
+        <div className='flex items-center gap-2'>
+          <HeadCountIcon className='size-6' />
+          <p>
+            {room.players}/{room.maxPlayers}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default RoomItem;
