f21c6496ca1ea965605960086bb807b3754fb05d
isemae <deleted.lee13@gmail.com>
2025-03-19 13:17:06 +0900

    refac: modal 오픈 로직 수정 및 컨텍스트 훅 네이밍 변경
    ```
      const { openModal } = useModal();
    
      const handleOpenModal = () => {
        openModal({
          title: '모달 제목',
          content: <p>모달 내용</p>,
          onConfirm: () => console.log('확인 버튼 클릭'),
          onCancel: () => console.log('취소 버튼 클릭'),
        });
      };
    ```
    와 같이 사용할 수 있도록 useModal 훅을 수정

--- frontend/web/src/components/ui/modal/Modal.tsx
+++ frontend/web/src/components/ui/modal/Modal.tsx
@@ -1,22 +1,13 @@
-import { ModalProps } from './modalContext';
+import { ModalProps } from './ModalProvider';
 
-export const Modal: React.FC<ModalProps> = ({ isOpen, children, onClose }) => {
-  if (!isOpen) return null;
-
+const Modal: React.FC<ModalProps> = ({ children }) => {
   return (
     <div className='bg-modal fixed inset-0 flex items-center justify-center'>
-      <div className='bg-background w-modal flex flex-col items-center justify-center rounded-lg p-6 shadow-lg'>
+      <div className='text-stroke bg-background w-modal flex flex-col items-center justify-center rounded-lg p-6 text-white'>
         {children}
-
-        <div>
-          <button className='bg-secondary mt-4 rounded border-black p-2 text-black' onClick={onClose}>
-            취소
-          </button>
-          <button className='bg-primary mt-4 rounded border-black p-2 text-white' onClick={onClose}>
-            확인
-          </button>
-        </div>
       </div>
     </div>
   );
 };
+
+export default Modal;
