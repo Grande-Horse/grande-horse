3f0d8c84dbb24d375e77425d73f0eb0b48180145
isemae <deleted.lee13@gmail.com>
2025-03-20 11:12:27 +0900

    fix: modal 컴포넌트에 전역 스타일링을 적용

--- frontend/web/src/main.tsx
+++ frontend/web/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import ModalProvider from './components/ui/modal/ModalProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <App />
   </StrictMode>
 );
