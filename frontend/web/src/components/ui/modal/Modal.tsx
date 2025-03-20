8d645cc30213762f3f608be99921bca4fa0976b9
isemae <deleted.lee13@gmail.com>
2025-03-20 11:02:14 +0900

    fix: modal 컴포넌트가 max-w를 사용하도록 변경

--- frontend/web/src/components/ui/modal/Modal.tsx
+++ frontend/web/src/components/ui/modal/Modal.tsx
@@ -3,7 +3,7 @@ import { ModalProps } from './ModalProvider';
 const Modal: React.FC<ModalProps> = ({ children }) => {
   return (
     <div className='bg-modal fixed inset-0 flex items-center justify-center'>
-      <div className='text-stroke bg-background w-modal flex flex-col items-center justify-center rounded-lg p-6 text-white'>
+      <div className='text-stroke bg-background max-w-modal flex flex-col items-center justify-center rounded-lg p-6 text-white'>
         {children}
       </div>
     </div>
