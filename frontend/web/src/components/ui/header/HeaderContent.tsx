79a881057eb94f7b760a4fbe12c9cfaca85e226f
김서로 <vholls2@naver.com>
2025-03-24 12:14:37 +0900

    feat: Header & BottomNavBar router 기능 추가

--- frontend/web/src/components/ui/header/HeaderContent.tsx
+++ frontend/web/src/components/ui/header/HeaderContent.tsx
@@ -1,6 +1,7 @@
 import BackIcon from '@/assets/icons/backArrowIcon.svg?react';
 import CoinIcon from '@/assets/icons/coinIcon.svg?react';
 import FootIcon from '@/assets/icons/footIcon.svg?react';
+import useInternalRouter from '@/hooks/useInternalRouter';
 
 interface CurrencyProps {
   coin: number;
@@ -11,48 +12,60 @@ interface InfoHeaderProps {
   title?: string;
 }
 
-const DefaultContent: React.FC<CurrencyProps> = ({ coin, foot }) => (
-  <>
-    <button className='cursor-pointer'>
-      <BackIcon />
-    </button>
+const DefaultContent: React.FC<CurrencyProps> = ({ coin, foot }) => {
+  const { goBack } = useInternalRouter();
 
-    <div className='flex gap-6'>
-      <div className='flex items-center justify-center gap-3'>
-        <CoinIcon />
-        <p className='text-stroke'>{coin.toLocaleString()}</p>
+  return (
+    <>
+      <button className='cursor-pointer' onClick={goBack}>
+        <BackIcon />
+      </button>
+
+      <div className='flex gap-6'>
+        <div className='flex items-center justify-center gap-3'>
+          <CoinIcon />
+          <p className='text-stroke'>{coin.toLocaleString()}</p>
+        </div>
+        <div className='flex items-center justify-center gap-3'>
+          <FootIcon />
+          <p className='text-stroke'>{foot.toLocaleString()}</p>
+        </div>
       </div>
-      <div className='flex items-center justify-center gap-3'>
-        <FootIcon />
-        <p className='text-stroke'>{foot.toLocaleString()}</p>
+    </>
+  );
+};
+
+const InfoContent: React.FC<InfoHeaderProps> = ({ title }) => {
+  const { goBack } = useInternalRouter();
+
+  return (
+    <div className='flex w-full items-center justify-between'>
+      <button className='cursor-pointer' onClick={goBack}>
+        <BackIcon />
+      </button>
+
+      <div className='flex'>
+        <p>{title ?? ''}</p>
       </div>
+      <div className='w-16'></div>
     </div>
-  </>
-);
+  );
+};
 
-const InfoContent: React.FC<InfoHeaderProps> = ({ title }) => (
-  <div className='flex w-full items-center justify-between'>
-    <button className='cursor-pointer'>
-      <BackIcon />
-    </button>
+const WaitingRoomContent: React.FC<InfoHeaderProps> = ({ title }) => {
+  const { goBack } = useInternalRouter();
 
-    <div className='flex'>
-      <p>{title ?? ''}</p>
-    </div>
-    <div className='w-16'></div>
-  </div>
-);
-
-const WaitingRoomContent: React.FC<InfoHeaderProps> = ({ title }) => (
-  <>
-    <button className='cursor-pointer'>
-      <BackIcon />
-    </button>
-    <div className='bg-secondary ml-10 flex flex-1 justify-center overflow-hidden rounded-xl border-1 p-3 text-black'>
-      <p className='line-clamp-1'>{title ?? '방 제목을 입력하세요'}</p>
-    </div>
-  </>
-);
+  return (
+    <>
+      <button className='cursor-pointer' onClick={goBack}>
+        <BackIcon />
+      </button>
+      <div className='bg-secondary ml-10 flex flex-1 justify-center overflow-hidden rounded-xl border-1 p-3 text-black'>
+        <p className='line-clamp-1'>{title ?? '방 제목을 입력하세요'}</p>
+      </div>
+    </>
+  );
+};
 
 const PastureContent: React.FC<CurrencyProps> = ({ coin, foot }) => (
   <>
