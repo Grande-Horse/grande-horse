4544bd8e5d0d573affd48e85fd2d8923aeb00308
신성우 <asdfgg77@naver.com>
2025-03-25 15:42:36 +0900

    chore: navbar가 필요 없는 페이지에서 렌더링 되지 않게 로직 구현 (확장성 고려 o)

--- frontend/web/src/components/ui/navbar/BottomNavBar.tsx
+++ frontend/web/src/components/ui/navbar/BottomNavBar.tsx
@@ -8,32 +8,32 @@ const BottomNavBar: React.FC = () => {
   const rootHoverStyle = 'hover:bg-primary/10';
 
   const excludedPaths = [{ path: '/racetrack/room' }, { path: '/racetrack/room/race' }];
-  const isNotRender = excludedPaths.findIndex((item) => pathname === item.path);
+  const isNotRender = excludedPaths.some((item) => pathname.startsWith(item.path));
   const isRoot = pathname === '/';
 
+  if (isNotRender) {
+    return null;
+  }
+
   return (
-    <>
-      {isNotRender === -1 && (
-        <nav
-          className={`${isRoot ? 'bg-transparent' : 'bg-background'} z-bottomnavbar sticky bottom-0 flex h-24 w-full items-center justify-around`}
-        >
-          {bottomNavBarItem.map((item, index) => {
-            const isActive = location.pathname === item.pathname || location.pathname.startsWith(item.pathname + '/');
-            return (
-              <Link
-                key={index}
-                to={item.pathname}
-                replace
-                className={`hover:bg-primary flex w-full flex-col items-center justify-between py-2 ${isActive ? activeStyle : ''} ${isRoot && isActive ? rootActiveStyle : ''} ${isRoot ? rootHoverStyle : ''}`}
-              >
-                <item.icon className='size-14' />
-                <p className='text-stroke text-detail2'>{item.text}</p>
-              </Link>
-            );
-          })}
-        </nav>
-      )}
-    </>
+    <nav
+      className={`${isRoot ? 'bg-transparent' : 'bg-background'} z-bottomnavbar sticky bottom-0 flex h-24 w-full items-center justify-around`}
+    >
+      {bottomNavBarItem.map((item, index) => {
+        const isActive = location.pathname === item.pathname || location.pathname.startsWith(item.pathname + '/');
+        return (
+          <Link
+            key={index}
+            to={item.pathname}
+            replace
+            className={`hover:bg-primary flex w-full flex-col items-center justify-between py-2 ${isActive ? activeStyle : ''} ${isRoot && isActive ? rootActiveStyle : ''} ${isRoot ? rootHoverStyle : ''}`}
+          >
+            <item.icon className='size-14' />
+            <p className='text-stroke text-detail2'>{item.text}</p>
+          </Link>
+        );
+      })}
+    </nav>
   );
 };
 
