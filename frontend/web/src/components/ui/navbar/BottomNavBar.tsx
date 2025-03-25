8a901b471546ecc5bc6cf275e92340d35e7f3e66
신성우 <asdfgg77@naver.com>
2025-03-25 12:40:20 +0900

    chore: 페이지에 따른 하단바 해당 요소 active 스타일 추가

--- frontend/web/src/components/ui/navbar/BottomNavBar.tsx
+++ frontend/web/src/components/ui/navbar/BottomNavBar.tsx
@@ -1,15 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { bottomNavBarItem } from '@/constants/bottomNavBarIcon';
 
 const BottomNavBar: React.FC = () => {
+  const location = useLocation();
+  const activeStyle = 'bg-primary';
+
   return (
     <nav className='bg-background z-bottomnavbar sticky bottom-0 flex h-24 w-full items-center justify-around'>
       {bottomNavBarItem.map((item, index) => {
+        const isActive = location.pathname === item.pathname || location.pathname.startsWith(item.pathname + '/');
         return (
           <Link
             key={index}
             to={item.pathname}
-            className='hover:bg-primary flex w-full flex-col items-center justify-between py-2'
+            className={`hover:bg-primary flex w-full flex-col items-center justify-between py-2 ${isActive ? activeStyle : ''}`}
           >
             <item.icon className='size-14' />
             <p className='text-stroke text-detail2'>{item.text}</p>
--- frontend/web/src/constants/bottomNavBarIcon.ts
+++ frontend/web/src/constants/bottomNavBarIcon.ts
@@ -6,6 +6,6 @@ import HorseDealIcon from '@/assets/icons/horseDealIcon.svg?react';
 export const bottomNavBarItem = [
   { icon: PastureIcon, text: '목장', pathname: '/' },
   { icon: StallIcon, text: '마구간', pathname: '/stall' },
-  { icon: RacetrackIcon, text: '경마장', pathname: 'racetrack' },
+  { icon: RacetrackIcon, text: '경마장', pathname: '/racetrack' },
   { icon: HorseDealIcon, text: '마시장', pathname: '/market' },
 ] as const;
