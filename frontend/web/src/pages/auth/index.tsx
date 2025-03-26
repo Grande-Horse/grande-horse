01d6e6ff1cc5e5dfc93b526cc878430d54d01095
isemae <deleted.lee13@gmail.com>
2025-03-26 18:05:36 +0900

    chore: 앱 타이틀 표시 컴포넌트 추가

--- frontend/web/src/pages/auth/index.tsx
+++ frontend/web/src/pages/auth/index.tsx
@@ -3,27 +3,43 @@ const AuthPage: React.FC = () => {
   const upperCloudSrc = 'src/assets/images/backgrounds/loginBgCloudUpper.png';
   const lowerCloudSrc = 'src/assets/images/backgrounds/loginBgCloudLower.png';
 
-  const upperCloud = () => (
+  const UpperCloud = () => (
     <div
       className='animate-moveCloudUpper absolute top-0 left-0 h-full w-full bg-[length:calc(2*100%)] bg-repeat-x'
       style={{ backgroundImage: `url(${upperCloudSrc})` }}
     />
   );
 
-  const lowerCloud = () => (
+  const LowerCloud = () => (
     <div
       className='animate-moveCloudLower absolute bottom-0 left-0 h-full w-full bg-[length:calc(2*100%)] bg-repeat-x'
       style={{ backgroundImage: `url(${lowerCloudSrc})` }}
     />
   );
 
+  const KakaoLogin = () => <button className='bg-kakao w-sm rounded-sm p-4 text-black'>카카오 로그인</button>;
+
+  const SsafyLogin = () => <button className='bg-ssafy w-sm rounded-sm p-4 text-black'>SSAFY 로그인</button>;
+
   return (
     <div
-      className='relative h-[calc(100vh-12rem)] w-full overflow-hidden bg-[length:calc(2*100%)] bg-repeat-x'
+      className='relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-[length:calc(2*100%)] bg-repeat-x'
       style={{ backgroundImage: `url(${landscapeSrc})` }}
     >
-      {upperCloud()}
-      {lowerCloud()}
+      <UpperCloud />
+      <LowerCloud />
+
+      <div className='min-h-1/3 min-w-1/2 flex-col items-center justify-center bg-[url("src/assets/images/appTitleBg.png")] bg-contain bg-center bg-no-repeat p-6'>
+        <h1 className='text-stroke text-heading1 flex w-full flex-col items-center justify-center p-20 text-nowrap'>
+          <span className='w-full text-left'>그런데</span>
+          <span className='w-full text-right'>말입니다</span>
+        </h1>
+      </div>
+
+      <div className='absolute bottom-0 flex w-full flex-col items-center justify-center'>
+        <KakaoLogin />
+        <SsafyLogin />
+      </div>
     </div>
   );
 };
