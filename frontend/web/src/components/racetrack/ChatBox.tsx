6ff4dbc8d1bcaab386e8087aeae06a840c04b755
신성우 <asdfgg77@naver.com>
2025-03-28 16:03:44 +0900

    feat: 채팅창 UI 구현 (채팅창 focus 이벤트 및 메시지 엔터키 입력을 위해 Button 컴포넌트에 forwardRef 추가)

--- frontend/web/src/components/racetrack/ChatBox.tsx
+++ frontend/web/src/components/racetrack/ChatBox.tsx
@@ -1,15 +1,86 @@
-import { Button } from '../ui/Button';
-import Input from '../ui/Input';
+import { useEffect, useRef, useState } from 'react';
+import { Button } from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
+import { rankTextColor } from '@/constants/rank';
+import { type RankType } from '@/types/horse';
+
+interface Chat {
+  id: number;
+  userName: string;
+  rank: RankType;
+  message: string;
+}
+
+const chatMockData: Chat[] = [
+  { id: 1, userName: '굿필승', rank: 'normal', message: '안녕하세요' },
+  { id: 2, userName: '굿굿필승', rank: 'rare', message: '안녕하세요' },
+  { id: 3, userName: '런스피드', rank: 'unique', message: '이 방이 핫하다고해서 들어왔습니다.' },
+  { id: 1, userName: '굿필승', rank: 'normal', message: '한 마리 더 들어오면 시작할게요.' },
+];
 
 const ChatBox: React.FC = () => {
+  const [chatContent, setChatContent] = useState<Chat[]>(chatMockData);
+  const [message, setMessage] = useState('');
+  const ulRef = useRef<HTMLUListElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  // 채팅을 치는 유저 아이디 (캐시되어있는 개인정보 이용)
+  const User = {
+    id: 1,
+    userName: '굿필승',
+    rank: 'normal' as RankType,
+  };
+
+  const handleEnterOnClick = () => {
+    setChatContent((prev) => {
+      return [...prev, { ...User, message }];
+    });
+    setMessage('');
+  };
+
+  const handleChatMessageOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleEnterOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      buttonRef.current?.click();
+    }
+  };
+
+  useEffect(() => {
+    if (ulRef.current) {
+      ulRef.current.scrollTop = ulRef.current.scrollHeight;
+    }
+  }, [chatContent]);
+
   return (
-    <div className='flex flex-1 flex-col gap-5'>
-      <div className='flex flex-1 items-center justify-center rounded-2xl bg-white/10 inset-shadow-xs inset-shadow-white/10'>
-        채팅
-      </div>
-      <div className='flex gap-3'>
-        <Input />
-        <Button variant='primary'>Enter</Button>
+    <div className='flex flex-1 flex-col gap-3 overflow-auto'>
+      <ul
+        ref={ulRef}
+        className='text-stroke flex flex-1 flex-col gap-3 overflow-auto rounded-2xl bg-white/10 px-5 py-3 inset-shadow-xs inset-shadow-white/10'
+      >
+        {chatContent.map((chat) => {
+          return (
+            <li key={`chat ${Math.random()}`} className='flex gap-3'>
+              <p className={`${rankTextColor[chat.rank]}`}>{chat.userName}:</p>
+              <p className='flex-1'>{chat.message}</p>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className='sticky bottom-0 flex gap-3'>
+        <Input
+          type='text'
+          onChange={handleChatMessageOnChange}
+          onKeyDown={handleEnterOnKeyDown}
+          placeholder='채팅을 입력하세요'
+          value={message}
+        />
+        <Button variant='primary' ref={buttonRef} onClick={handleEnterOnClick}>
+          Enter
+        </Button>
       </div>
     </div>
   );
