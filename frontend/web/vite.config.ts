1c85f0d9b306fa824de74acba956908c139e4cc4
우성문 <tjdans7665@naver.com>
2025-03-28 11:16:59 +0900

    chore: front file modify

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
-import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
   server: {
     port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost+2.pem')),
-    },
   },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
