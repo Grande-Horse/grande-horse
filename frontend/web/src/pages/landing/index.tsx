c8cb6d66d436138d71c19d6825e2b4c523ba7df0
isemae <deleted.lee13@gmail.com>
2025-03-31 16:39:23 +0900

    fix: 랜딩페이지 배경화면 경로를 import로 사용하도록 수정

--- frontend/web/src/pages/landing/index.tsx
+++ frontend/web/src/pages/landing/index.tsx
@@ -1,14 +1,14 @@
 import kakaoLogo from '@/assets/images/kakao-logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ssafyLogo from '@/assets/images/ssafy-logo.png';
 import { useEffect, useContext } from 'react';
 import { AuthContext } from '../register/AuthContextProvider';
+import appTitleBg from '@/assets/images/appTitleBg.png';
+import landingBgLandscape from '@/assets/images/backgrounds/landingBgLandscape.png';
+import landingBgCloudUpper from '@/assets/images/backgrounds/landingBgCloudUpper.png';
+import landingBgCloudLower from '@/assets/images/backgrounds/landingBgCloudLower.png';
 
 const LandingPage: React.FC = () => {
-  const landscapeSrc = 'src/assets/images/backgrounds/landingBgLandscape.png';
-  const upperCloudSrc = 'src/assets/images/backgrounds/landingBgCloudUpper.png';
-  const lowerCloudSrc = 'src/assets/images/backgrounds/landingBgCloudLower.png';
-
   interface CloudBgProps {
     position: 'upper' | 'lower';
   }
@@ -16,7 +16,7 @@ const LandingPage: React.FC = () => {
   const CloudBg = ({ position }: CloudBgProps) => (
     <div
       className={`animate-moveCloud${position.charAt(0).toUpperCase() + position.slice(1)} absolute top-0 left-0 h-screen w-full bg-cover bg-repeat-x`}
-      style={{ backgroundImage: `url(${position === 'upper' ? upperCloudSrc : lowerCloudSrc})` }}
+      style={{ backgroundImage: `url(${position === 'upper' ? landingBgCloudUpper : landingBgCloudLower})` }}
     />
   );
 
@@ -38,7 +38,9 @@ const LandingPage: React.FC = () => {
 
   const TitlePanel = () => {
     return (
-      <div className='absolute top-0 left-1/2 aspect-300/278 w-2/3 -translate-x-1/2 transform bg-[url("src/assets/images/appTitleBg.png")] bg-contain bg-center bg-no-repeat'>
+      <div
+        className={`absolute top-0 left-1/2 aspect-300/278 w-2/3 -translate-x-1/2 transform bg-[url(${appTitleBg})] bg-contain bg-center bg-no-repeat`}
+      >
         <div className='absolute top-[70%] right-0 left-0 -translate-y-1/2'>
           <h1 className='text-stroke text-heading1 flex flex-col items-center justify-center px-20 text-nowrap'>
             <span className='w-full text-start'>그런데</span>
@@ -63,7 +65,7 @@ const LandingPage: React.FC = () => {
   return (
     <div
       className='relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-cover bg-repeat-x'
-      style={{ backgroundImage: `url(${landscapeSrc})` }}
+      style={{ backgroundImage: `url(${landingBgLandscape})` }}
     >
       <CloudBg position='upper' />
       <CloudBg position='lower' />
