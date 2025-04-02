06e14ca636d308f21c9ca9b6e448db1119be52fa
신성우 <asdfgg77@naver.com>
2025-04-02 18:46:37 +0900

    chore: 경주 로직 설계 진행 중

--- frontend/web/src/components/racetrack/Race.tsx
+++ frontend/web/src/components/racetrack/Race.tsx
@@ -2,34 +2,51 @@ import raceTopBg from '@/assets/images/backgrounds/raceTopBg.webp';
 import raceLineBg from '@/assets/images/backgrounds/raceLineBg.webp';
 
 import Horse from '@/components/racetrack/Horse';
+import { useEffect, useState } from 'react';
 
 const Race: React.FC = () => {
+  const [changePosition, setChangePosition] = useState(0);
+  const [h1, setH1] = useState(0);
+
+  useEffect(() => {
+    setChangePosition((prev) => {
+      if (prev >= 100) {
+        return prev;
+      } else {
+        return prev + 0.005;
+      }
+    });
+  }, [changePosition]);
+
+  const randomBox = [-1, 1];
+
+  useEffect(() => {
+    if (changePosition >= 100) {
+      setH1(300);
+    } else {
+      setH1((prev) => prev + 10 * randomBox[Math.floor(Math.random() * 2)]);
+    }
+  }, [changePosition]);
+
   return (
     <div className='flex h-full w-full flex-col justify-center bg-[#55da54]'>
-      <div className='flex h-full flex-col items-center justify-center'>
-        <div className='flex h-40 w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceTopBg})` }}></div>
+      {/* 상단 배경 */}
+      <div
+        className='flex h-60 w-full flex-col bg-cover bg-no-repeat'
+        style={{ backgroundImage: `url(${raceTopBg})`, backgroundPosition: `${changePosition}% 0` }}
+      ></div>
 
-        <div className='flex h-full w-full flex-1 flex-col items-center justify-center'>
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
+      {/* 레이스 */}
+      <div className='flex h-full w-full flex-col items-center justify-evenly'>
+        <div
+          className='flex w-full bg-cover bg-no-repeat'
+          style={{
+            backgroundPosition: `${changePosition}% 0`,
+            backgroundImage: `url(${raceLineBg})`,
+          }}
+        >
+          <div style={{ transform: `translateX(${h1}px)`, transition: 'all 3s' }}>
             <Horse id='fwefwef' color='black' type='auto' direction='change' />
-          </div>
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
-            <Horse id='dsfsdfds' color='brown' />
-          </div>
-
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
-            <Horse id='dsfsdfasddsad' color='darkbrown' />
-          </div>
-
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
-            <Horse id='dsfsdafasdfasdfasdfasd' color='gray' />
-          </div>
-
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
-            <Horse id='fsdfdsfdsfdsf' color='lightbrown' />
-          </div>
-          <div className='flex w-full bg-cover bg-no-repeat' style={{ backgroundImage: `url(${raceLineBg})` }}>
-            <Horse id='fsdfasdfsdafasdfasdfasdfsda' color='black' />
           </div>
         </div>
       </div>
