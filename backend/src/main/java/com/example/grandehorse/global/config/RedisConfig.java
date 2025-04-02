56a28d4f42c63166500feb0d99628ba3ba9683d9
kyoungbeom <pkb1998@naver.com>
2025-04-02 10:04:13 +0900

    chore: config 파일 패키지 수정

--- backend/src/main/java/com/example/grandehorse/global/config/RedisConfig.java
+++ backend/src/main/java/com/example/grandehorse/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.grandehorse.infrastructure.config;
+package com.example.grandehorse.global.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
