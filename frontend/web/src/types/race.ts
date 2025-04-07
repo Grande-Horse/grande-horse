79ab73505c081d427dab9a2a157190dd0945c457
서로 <vholls2@naver.com>
2025-04-07 13:51:49 +0900

    feat: 경기 결과 컴포넌트 구현

--- frontend/web/src/types/race.ts
+++ frontend/web/src/types/race.ts
@@ -5,3 +5,11 @@ export interface RaceRecordType {
   totalRaces?: number;
   totalPrize?: number;
 }
+
+export interface RaceResultType {
+  prize: number;
+  userName: string;
+  horseName: string;
+  coin: number;
+  time: number;
+}
