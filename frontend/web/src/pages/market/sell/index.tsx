db17d173fb05572c087a1cfbbbc4a665076a9ac6
okxooxoo <okxooxoo@naver.com>
2025-04-08 14:00:00 +0900

    fix: 판매 페이지로 이동하지 않는 버그 픽스

--- frontend/web/src/pages/market/sell/index.tsx
+++ frontend/web/src/pages/market/sell/index.tsx
@@ -1,9 +1,6 @@
 import HorseProfileCard from '@/components/cards/HorseProfileCard';
 import CoinIcon from '@/assets/icons/coinIcon.svg?react';
 import { Button } from '@/components/ui/Button';
-import { useState } from 'react';
-import { HorseType } from '@/types/horse';
-import { horseMockData } from '@/mocks/datas/horse';
 import { sellHorse } from '@/services/trading';
 import Tabs from '@/components/ui/Tabs';
 import { sellTabList } from '@/constants/tabList';
@@ -13,19 +10,14 @@ import PriceHistoryPanel from '@/components/market/panels/PriceHistoryPanel';
 
 const SellPage: React.FC = () => {
   const location = useLocation();
-  const state = location.state;
-
-  console.log(state);
-
-  // TODO: 해당 말 카드의 정보 불러오기
-  // const [horse] = useState<HorseType>(horseMockData);
+  const horse = location.state;
 
   const handleSellHorse = async () => {
     try {
-      // await sellHorse({
-      //   cardId: Number(cardId),
-      //   price: 500,
-      // });
+      await sellHorse({
+        cardId: Number(horse.cardId),
+        price: 500,
+      });
     } catch (error) {
       console.error(error);
     }
