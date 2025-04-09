8f2eb911c5177964b6f843d247152ee1ecefff08
우성문 <tjdans7665@naver.com>
2025-04-09 14:45:56 +0900

    chore: localdatetime setting

--- backend/src/main/java/com/example/grandehorse/global/config/WebConfig.java
+++ backend/src/main/java/com/example/grandehorse/global/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.example.grandehorse.global.config;
 
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
