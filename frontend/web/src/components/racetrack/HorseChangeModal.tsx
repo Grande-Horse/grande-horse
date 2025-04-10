25a91045997ace7ff5509ced60fd34cd7f980d8f
신성우 <asdfgg77@naver.com>
2025-04-10 10:25:55 +0900

    feat: 경기 대기방 대표말 변경 추가

--- frontend/web/src/components/racetrack/HorseChangeModal.tsx
+++ frontend/web/src/components/racetrack/HorseChangeModal.tsx
@@ -4,13 +4,15 @@ import SelectedIcon from '@/assets/icons/selectedIcon.svg?react';
 import Horse from '@/components/racetrack/Horse';
 import { Button } from '@/components/ui/Button';
 
-import { userMockData } from '@/mocks/datas/user';
+import { type CandidateHorseType } from '@/types/horse';
 
 interface HorseChangeModalProps {
-  close: (value: unknown) => void;
+  onSuccess: (data: CandidateHorseType[]) => void;
+  horseList: CandidateHorseType[];
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ close }) => {
+const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ onSuccess, horseList, setIsOpen }) => {
   const bgImageClass = {
     all: '',
     normal: `bg-[url(@/assets/images/backgrounds/normalBg.webp)]`,
@@ -22,7 +24,7 @@ const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ close }) => {
 
   const [selectedHorseId, setSelectedHorseId] = useState<string | null>(null);
 
-  const [activeHorseStates, setActiveHorseStates] = useState<boolean[]>(new Array(userMockData.length).fill(false));
+  const [activeHorseStates, setActiveHorseStates] = useState<boolean[]>(new Array(horseList.length).fill(false));
 
   const handleCardClick = (horseId: string) => {
     setSelectedHorseId((prevId) => (prevId === horseId ? null : horseId));
@@ -40,12 +42,25 @@ const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ close }) => {
     });
   };
 
+  const handleChangeClick = () => {
+    onSuccess(horseList.filter((horse) => String(horse.horseId) === selectedHorseId));
+    setSelectedHorseId(null);
+    setActiveHorseStates(new Array(horseList.length).fill(false));
+    setIsOpen(false);
+  };
+
+  const handleCancelClick = () => {
+    setSelectedHorseId(null);
+    setActiveHorseStates(new Array(horseList.length).fill(false));
+    setIsOpen(false);
+  };
+
   return (
-    <>
+    <div className='bg-background max-w-modal flex w-full flex-col items-center justify-center rounded-lg px-10 py-14 text-white'>
       <div className='text-heading4 text-stroke pb-5 font-normal'>대표말 변경</div>
       <div className='mb-4 grid grid-cols-3 gap-5'>
-        {userMockData.map((horse, idx) => {
-          const isSelected = String(horse.id) === selectedHorseId;
+        {horseList.map((horse, idx) => {
+          const isSelected = String(horse.horseId) === selectedHorseId;
           const isActiveHorse = activeHorseStates[idx];
           return (
             <div className='relative' key={`change ${idx}`}>
@@ -59,18 +74,18 @@ const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ close }) => {
               )}
               <div
                 key={`horse ${idx}`}
-                onClick={() => handleCardClick(String(horse.id))}
+                onClick={() => handleCardClick(String(horse.horseId))}
                 onMouseLeave={() => setActiveHorse(idx, false)}
                 onMouseEnter={() => setActiveHorse(idx, true)}
                 onTouchStart={() => setActiveHorse(idx, true)}
-                className={`${bgImageClass[horse.rank]} outline-secondary flex cursor-pointer flex-col items-center justify-center rounded-2xl bg-cover bg-center pt-5 hover:outline-4 hover:outline-dashed active:outline-dashed`}
+                className={`${bgImageClass[horse.horseRank]} outline-secondary flex cursor-pointer flex-col items-center justify-center rounded-2xl bg-cover bg-center pt-5 hover:outline-4 hover:outline-dashed active:outline-dashed`}
               >
                 <p className='text-detail2 text-stroke flex w-full items-center justify-center rounded-xl shadow-inner shadow-black/20'>
-                  {horse.userName}
+                  {horse.name}
                 </p>
                 <Horse
                   color={horse.coatColor}
-                  key={horse.id}
+                  key={horse.horseId}
                   direction='right'
                   state={isActiveHorse ? 'run' : 'idle'}
                 />
@@ -80,14 +95,14 @@ const HorseChangeModal: React.FC<HorseChangeModalProps> = ({ close }) => {
         })}
       </div>
       <div className='flex w-full gap-3 pt-5'>
-        <Button className='flex-1' onClick={close}>
+        <Button className='flex-1' onClick={handleCancelClick}>
           취소
         </Button>
-        <Button className='flex-1' onClick={() => {}}>
+        <Button className='flex-1' onClick={handleChangeClick}>
           변경
         </Button>
       </div>
-    </>
+    </div>
   );
 };
 
